57418207937db6ceeb9321c71a0f8e35c5f2e210
ms2sato <ms2.sato@gmail.com>
2016-05-19 22:22:53 +0900

    draft

--- js/waitron.js
+++ js/waitron.js
@@ -10,8 +10,121 @@
   /* eslint-enable no-undef */
 })(typeof window !== 'undefined' ? window : this, function (global, undefined) { // eslint-disable-line
   'use strict'
-
   /*global $ _ */
+
+
+  function log() {
+    console.log.apply(console, arguments)
+  }
+
+  function each (o, func) {
+    if (Array.isArray(o)) {
+      for (var i = 0; i < o.length; ++i) { func.call(o, o[i], i) }
+      return o
+    }
+
+    for (var key in o) {
+      if (o.hasOwnProperty(key)) func.call(o, o[key], key)
+    }
+    return o
+  }
+
+  var me = function (el) {
+    return {
+      el: el,
+      on: function (eventName, listener) {
+        return addEventListener(this, eventName, listener)
+      },
+      render: function () {
+        $(this.el).html(this.template(this))
+      }
+    }
+  }
+
+  function onNewProperty(o, prop, newValue) {
+    log('onNewProperty', arguments)
+  }
+
+  function onBeforeChange(o, prop, value, newValue) {
+    if (!(prop in o)) {
+      onNewProperty(o, prop, newValue)
+    }
+
+    log('onBeforeChange', arguments)
+  }
+  function onAfterChange(o, prop, value, oldValue) {
+    log('onAfterChange', arguments)
+
+    me.tickContext.push(o.id, function () {
+      o.update()
+    });
+  }
+
+  function listenateProp (o, prop) {
+    var value = o[prop]
+    Object.defineProperty(o, prop, {
+      get: function () { return value },
+      set : function (newValue) {
+        var oldValue = value
+        onBeforeChange(o, prop, value, newValue)
+        value = newValue
+        onAfterChange(o, prop, value, oldValue)
+      },
+      enumerable : true,
+      configurable : true
+    })
+  }
+
+  // TODO: unenhance
+
+  var id_counter = 1;
+  me.enhance = function enhance (o) {
+    if (o.__uniqueId === undefined) {
+      // @see http://stackoverflow.com/questions/1997661/unique-object-identifier-in-javascript
+      Object.defineProperty(o, "__uniqueId", {
+          writable: true
+      })
+
+      o.__uniqueId = id_counter++
+
+      Object.defineProperty(o, "id", {
+          get: function() {
+              return this.__uniqueId
+          }
+      })
+    }
+
+    Object.defineProperty(o, "listeners", {
+      writable: true,
+      value: {},
+      enumerable : false,
+      configurable : true
+    })
+    each(o, function (value, key) {
+      if (key == "el" || key == "on" || key == "render") return
+
+      listenateProp(o, key)
+      o.listeners[key] = [] // FIXME: 効率化
+    })
+
+    o.onChange = function (key, listener) {
+      if (Array.isArray(key)) {
+        each(key, function (k) {
+          o.listeners[k].push(listener)
+        })
+      } else {
+        o.listeners[key].push(listener)
+      }
+    }
+
+    o.update = function () {
+      each(o.listeners, function (listenerArr) {
+        each(listenerArr, function (l) { l() })
+      })
+    }
+
+    return Object.keys(o.listeners)
+  }
 
   // @see http://stackoverflow.com/questions/8403108/calling-eval-in-particular-context
   function evalInContext (js, context) {
@@ -25,41 +138,79 @@
     /* eslint-enable no-eval */
   }
 
-  var me = function (el) {
-    return {
-      el: el,
-      on: function (ev, func) {
-        return addEventListener(this, ev, func)
-      },
-      update: function () {
-        $(this.el).html(this.template(this))
+  function addEventListener (scope, eventName, listener) {
+    var hand = function (event) {
+      try {
+        me.onBeforeEvent(scope, eventName, event, listener)
+        var ret = listener(event)
+        return me.onAfterEvent(scope, eventName, event, listener, ret)
+      } catch (ex) {
+        me.onEventError(ex)
+      } finally {
+        me.onEventFinally(scope, eventName, event, listener)
       }
     }
-  }
-
-  function addEventListener (w, ev, fun) {
-    var hand = function (e) {
-      me.onBeforeEvent(w, ev, e, fun)
-      var ret = fun(e)
-      return me.onAfterEvent(w, ev, e, fun, ret)
+    if (scope.el.addEventListener) {
+      return scope.el.addEventListener(eventName, hand, true)
     }
-    if (w.el.addEventListener) {
-      return w.el.addEventListener(ev, hand, true)
-    }
-    if (w.el.attachEvent) {
-      return w.el.attachEvent(ev, hand)
+    if (scope.el.attachEvent) {
+      return scope.el.attachEvent(eventName, hand)
     }
   };
 
-  me.onBeforeEvent = function () {}
+  var TickContext = (function () {
 
-  me.onAfterEvent = function (w, ev, e, fun, ret) {
-    console.log(ev, e, fun, ret)
-    if (ret && ret.skipUpdate === true) {
-      return ret.value
+    function TickContext (scope, eventName, event, listener) {
+      this.scope = scope
+      this.eventName = eventName
+      this.event = event
+      this.listener = listener
+      this.actions = {}
     }
-    w.update()
+
+    TickContext.prototype.execute = function () {
+      var self = this
+      each(this.actions, function (value, key) {
+        self.actions[key].call(self)
+      })
+    }
+
+    TickContext.prototype.push = function (key, func) {
+      this.actions[key] = func
+    }
+
+    TickContext.prototype.valueOf = function (key) {
+      return this.actions[key]
+    }
+
+    TickContext.prototype.clear = function () {
+      var self = this
+      each(this.actions, function (value, key) {
+        delete self.actions[key]
+      })
+    }
+
+    return TickContext
+  })();
+
+
+  me.onBeforeEvent = function (scope, eventName, event, listener) {
+    me.tickContext = new TickContext(scope, eventName, event, listener)
+  }
+
+  me.onAfterEvent = function (scope, eventName, event, listener, ret) {
+    log("onAfterEvent", eventName, event, listener, ret)
+    me.tickContext.execute()
     return ret
+  }
+
+  me.onEventError = function (ex) {
+    log(ex, ex.stack)
+    throw ex
+  }
+
+  me.onEventFinally = function (scope, eventName, event, listener) {
+    me.tickContext.clear()
   }
 
   var scriptsSelector = 'script[type="text/waitron"]'
@@ -76,18 +227,20 @@
     Component.prototype.init = function (el) {
       el = el || document.createElement('div')
 
-      var scripts = this.componentType.scripts
       var templates = this.componentType.templates
-      var w = me(el)
-      evalInContext(scripts, w)
+      var scope = me(el)
+      if (this.componentType.bootstrap(scope) !== false) {
+        var attrs = me.enhance(scope)
+        scope.onChange(attrs, function onChangeListener () {
+          scope.render()
+        })
+      }
 
-      this.w = w
-      w.template = _.template(templates)
-      w.update()
-    }
+      this.scope = scope
+      scope.template = _.template(templates)
+      scope.render()
 
-    Component.prototype.mixTo = function (el) {
-
+      return this
     }
 
     function ComponentType (scripts, templates) {
@@ -108,8 +261,15 @@
     ComponentType.prototype.createAfter = function (el) {
       var component = this.create()
       component.init()
-      $(el).after(component.w.el)
+      $(el).after(component.scope.el)
       return component
+    }
+
+    ComponentType.prototype.bootstrap = function (scope) {
+      if (typeof this.scripts === 'function') {
+        return this.scripts.call(scope)
+      }
+      evalInContext(this.scripts, scope)
     }
 
     return ComponentType
@@ -127,6 +287,29 @@
       componentType.createAfter(this)
     })
   }
+
+  me.addType = function (el, handler) {
+    return new ComponentType(handler, el)
+  }
+
+  var o = {}
+  o.test = 111
+  o.test2 = {aa: "sss"}
+
+  me.enhance(o)
+
+  me.onBeforeEvent(null, 'bootstrap', null, null)
+
+  o.test = 222
+  o.test2 = {bb: "111"}
+
+  o.onChange("test", function () {
+    console.log('enhanced test called')
+  })
+
+  o.test = 333
+
+  me.onAfterEvent(null, 'bootstrap', null, null)
 
   return me
 })
